39c455b81c7e2dd1e691d68c9320fc8f67a2042c
fengma_312 <fengma_312@126.com>
2022-10-30 19:28:37 +0800

    a

---
+++ TestClient/web.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SocketCore.Client;
+using SocketCore.Server;
+
+namespace TestClient;
+
+public class Web
+{
+    public void WebClient()
+    {
+        WebClients udpclient = new WebClients("127.0.0.1");
+        udpclient.OnOpen += () =>
+        {
+            Console.WriteLine("客户端接收长度：");
+        };
+        udpclient.OnReceive += (byte[] arg2) =>
+        {
+            Console.WriteLine("客户端接收长度：" + arg2.Length);
+            //udpServer.Send( arg2, arg3, arg4);
+        };
+        udpclient.OnClose += () =>
+        {
+
+        };
+        udpclient.Open(1).Wait();
+
+    }
+}
