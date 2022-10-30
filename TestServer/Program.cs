ffd8646566f000487992b945821eb5a85765b536
fengma_312 <fengma_312@126.com>
2022-10-30 16:16:26 +0800

    a

---
+++ TestServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace test.client;
+
+public class Program
+{
+    public static ILogger logger = null!;
+
+    static void Main(string[] args)
+    {
+        using ILoggerFactory loggerFactory =
+           LoggerFactory.Create(builder =>
+                builder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "hh:mm:ss ";
+                }));
+
+        logger = loggerFactory.CreateLogger("");
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
