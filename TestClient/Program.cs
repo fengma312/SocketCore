a007d9229435fe3ef3366823b8afd0f9f08e6645
fengma_312 <fengma_312@126.com>
2022-10-30 16:13:46 +0800

    a

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace test.client;
+
+public class Program
+{
+    public static ILogger logger = null!;
+
+    static void Main(string[] args)
+    {
+        using ILoggerFactory loggerFactory =
+           LoggerFactory.Create(builder =>
+                builder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "hh:mm:ss ";
+                }));
+
+        logger = loggerFactory.CreateLogger("");
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
