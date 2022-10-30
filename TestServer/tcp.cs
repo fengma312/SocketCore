83daf36d65e2c98a50ab8c6829f0c27ec3c71a60
fengma_312 <fengma_312@126.com>
2022-10-30 16:50:39 +0800

    a

--- TestServer/tcp.cs
+++ TestServer/tcp.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SocketCore.Server;
 
 namespace test.client;
 
 public class tcp
 {
-    
+    /// <summary>
+    /// 设置基本配置
+    /// </summary>   
+    /// <param name="numConnections">同时处理的最大连接数</param>
+    /// <param name="receiveBufferSize">用于每个套接字I/O操作的缓冲区大小(接收端)</param>
+    /// <param name="overtime">超时时长,单位秒.(每10秒检查一次)，当值为0时，不设置超时</param>
+    /// <param name="port">端口</param>
+    /// <param name="headerFlag">包头</param>
+    public void Pack(int numConnections, int receiveBufferSize, int overtime, int port, uint headerFlag)
+    {
+        TcpPackServer server = new TcpPackServer(numConnections, receiveBufferSize, overtime, headerFlag);
+        server.OnAccept += (int obj) =>
+        {
+            //server.SetAttached(obj, 555);
+            //Console.WriteLine($"Pack已连接{obj}");
+        };
+        server.OnReceive += (int arg1, byte[] arg2) =>
+        {
+            //int aaa = server.GetAttached<int>(arg1);
+            //Console.WriteLine($"Pack已接收:{arg1} 长度:{arg2.Length}");          
+            server.Send(arg1, arg2, 0, arg2.Length);
+        };
+        server.OnSend += (int arg1, int arg2) =>
+        {
+
+        };
+        server.OnClose += (int obj) =>
+        {
+            //int aaa = server.GetAttached<int>(obj);
+            //Console.WriteLine($"Pack断开{obj}");
+        };
+        server.Start(port);
+
+    }
+
+
+
+
+
 }
